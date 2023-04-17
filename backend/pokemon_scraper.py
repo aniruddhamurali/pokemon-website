ea10c8e629daf328e447af4f04399ba73ac67e1e
Aniruddha <aniruddha.murali@gmail.com>
2023-04-17 16:01:04 -0400

    Scraped and stored pokemon learnset data

--- backend/pokemon_scraper.py
+++ backend/pokemon_scraper.py
@@ -12,13 +12,23 @@ with open('mongodb_creds', 'r') as file:
 mongo = MongoClient("mongodb+srv://{}:{}@pokemon.vujdsli.mongodb.net/test".format(username, password))
 db = mongo["pokemon"]
 pokemon = db["pokemon"]
+pokemon_official_list = db["pokemon_official_list"]
 
+
+'''
 with open('data/pokedex.txt') as file:
     data = json.loads(file.read())
+    official_pokemon = []
 
     for key in data:
+        # Exclude unofficial pokemon
+        if int(data[key]["num"]) <= 0:
+            continue
+
+        
         pokedict = {
             "name": data[key]["name"],
+            "lowercase_name": key,
             "number": data[key]["num"],
             "types": data[key]["types"],
             "abilities": data[key]["abilities"],
@@ -34,5 +44,67 @@ with open('data/pokedex.txt') as file:
         }
         if "genderRatio" in data[key]:
             pokedict["genderRatio"] = data[key]["genderRatio"]
-        x = pokemon.update_one(pokedict)
+
+        x = pokemon.insert_one(pokedict)
+        official_pokemon.append(key)
     
+    y = pokemon_official_list.insert_one({"official_pokemon": official_pokemon})
+'''
+
+
+
+# Scrape pokemon learnsets and update pokemon entries in database
+with open('data/learnsets.txt') as file:
+    official_pokemon = list(pokemon_official_list.find())[0]['official_pokemon']
+    data = json.loads(file.read())
+    for pmon in data:
+        if pmon not in official_pokemon:
+            continue
+
+        learnset = {
+            "levelup": [],
+            "TM/HM": [],
+            "eggmoves": [],
+            "tutor": []
+        }
+
+        # Pokemon might be event-only
+        if "learnset" not in data[pmon]:
+            continue
+        
+        for move in data[pmon]["learnset"]:
+            # Only pick move learning methods from either gens 9 or 8
+            gen = int(data[pmon]["learnset"][move][0][0])
+
+            for method in data[pmon]["learnset"][move]:
+                # If gen 9, only add gen 9 learning methods
+                if int(method[0]) == 9 and gen == 9:
+                    if method[1] == "L":
+                        learnset["levelup"].append({"move": move, "level": int(method[2:])})
+                    elif method[1] == "M":
+                        learnset["TM/HM"].append(move)
+                    elif method[1] == "E":
+                        learnset["eggmoves"].append(move)
+                    elif method[1] == "T":
+                        learnset["tutor"].append(move)
+                
+                # If gen 8, only add gen 8 learning methods
+                elif int(method[0]) == 8 and gen == 8:
+                    if method[1] == "L":
+                        learnset["levelup"].append({"move": move, "level": int(method[2:])})
+                    elif method[1] == "M":
+                        learnset["TM/HM"].append(move)
+                    elif method[1] == "E":
+                        learnset["eggmoves"].append(move)
+                    elif method[1] == "T":
+                        learnset["tutor"].append(move)
+                
+                # Exclude gens 7 and earlier
+                else:
+                    break
+        
+        # Sort levelup moves in order of levels
+        learnset["levelup"] = sorted(learnset["levelup"], key=lambda x: x["level"])
+        query = {"lowercase_name": pmon}
+        newvalues = { "$set": {"learnset": learnset} }
+        x = pokemon.update_one(query, newvalues)
